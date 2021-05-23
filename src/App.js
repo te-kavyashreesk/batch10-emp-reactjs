36eb48bb3e0f5178948196deb61e45190ee8ff98
te-kavyashreesk <kavyashreesk14@gmail.com>
2021-05-23 22:27:01 +0530

    assigment emp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { routing } from "./Employee/routing";
+
+function App() {
+  return <BrowserRouter>
+                
+                {routing}
+                
+          </BrowserRouter>
+}
+
+export default App;
